dd0ee5d9f644434dfd8891822f71113b92f35089
lcc2207 <lynn@fidmail.com>
2018-07-26 13:22:31 -0500

    docker-compose

--- relaunch.sh
+++ relaunch.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 set -e
 
-dockername='snow-webhook'
-
-docker stop $dockername || true
-docker rm $dockername || true
-
-docker build -t $dockername \
-    --build-arg http_proxy=$http_proxy \
-    --build-arg https_proxy=$https_proxy \
-    .
-
-docker-compose up -d
+docker-compose down
+docker-compose up --build -d
