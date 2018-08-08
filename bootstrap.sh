eed0aca400cb5f6d8a51434cb9f19ba4109a6ad4
lcc2207 <lynn@fidmail.com>
2018-08-08 09:25:42 -0500

    update bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+apt update
+apt install python-pip -y
+
+pip install docker-compose
 
 curl -fsSL https://get.docker.com/ | sh
 service docker start || systemctl start docker
 
-mkdir -p /opt/snow-webhook/
-# git clone 
-# cp webhook.py /opt/webhook
+git clone git clone https://github.com/scalr-tutorials/scalr-snow-webhook.git /opt/snow-webhook
